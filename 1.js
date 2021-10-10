14fb517290cfc9648161284cb17751707ccb9941
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-10 21:33:36 +0800

    Edited number 1, 1.js file and README.md file --> PURE FUNCTIONS

--- 1.js
+++ 1.js
@@ -2,25 +2,15 @@
 const countries = [{ name: 'Philiippines', population: 200 }, { name: 'Japan', population: 30000 }, { name: 'china', population: 50000 }, { name: 'india', population: 3500 }];
 
 
-function getLowestPop(countries) {
-    let Lowest;
-
-    function hasLowPop(parameterCountry) {
-        Lowest = Lowest ?? parameterCountry;
-        return Lowest.population > parameterCountry.population;
+const getLowestPop = (countries) => {
+    const hasLowPop = (previous, current) => {
+        return previous.population > current.population ? current : previous;
     }
-
-    function compare(parameterCountry) {
-        if (hasLowPop(parameterCountry)) {
-            Lowest = parameterCountry;
-        }
-    }
-    countries.map(bit => compare(bit));
-    return Lowest
+    return countries.reduce(hasLowPop);
 }
 
 
-function getTotalPop(countries) {
+const getTotalPop = (countries) => {
     const getSum = (previous, current) => previous + current.population
     return countries.reduce(getSum, 0);
 }
