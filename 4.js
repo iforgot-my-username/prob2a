65b6ba955605141bc347e037c7342419c9c048f7
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-13 13:57:44 +0800

    Edited number 4, 4.js file into pure function

--- 4.js
+++ 4.js
@@ -1,53 +1,52 @@
-const studentList = [
-    { firstName: 'Juan', lastName: 'Dela cruz', idNum: 20047798, course: 'BSSE', year: 2 },
-    { firstName: 'Pedro', lastName: 'Luna', idNum: 19039796, course: 'BSSS', year: 3 },
-    { firstName: 'Luffy', lastName: 'Monkey', idNum: 21152364, course: 'BSIT', year: 1 },
-    { firstName: 'Ace', lastName: 'Protgas', idNum: 20058843, course: 'BSSS', year: 2 },
-    { firstName: 'Juan Pedro', lastName: 'Luna', idNum: 20036723, course: 'BSN', year: 2 },
-    { firstName: 'John', lastName: 'Snow', idNum: 21095467, course: 'BSA', year: 1 }
-]
-
-//A student has  ID #, name, course, and year
-
-
-function getStudentsInYear(year, studentList) {
+const getStudentsInYear = (year, studentList) => {
     const getYearLevel = student => student.year === year;
     return studentList.filter(getYearLevel);
 }
 
 
-String.prototype.removeSpaces = function removeSpaces() {
-    return this.trim().split(' ').join('');
+const removeSpaces = (str) => {
+    return str.trim().split(' ').join('');
 }
 
-function generateAccounts(studentList) {
-    function generateEmail(student) {
-        const firstName = student.firstName.removeSpaces();
-        const lastName = student.lastName.removeSpaces();
-        const idNum = student.idNum.toString().slice(0, 2)
-        return firstName.toLowerCase() + '.' + lastName.toLowerCase() + idNum + '@cpu.edu.ph'
-        //students first name, a dot, his last name, and the first two digits of his/her ID number, @cpu.edu.ph
+const randomLetter = (randomizer) => {
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    return letters[Math.trunc(randomizer * letters.length)];
+}
+
+const randomPunctutation = (randomizer) => {
+    const punctutations = ['.', '!', '?'];
+    return punctutations[Math.trunc(randomizer * 3)];
+};
+
+const randomDigit = (randomizer) => Math.trunc(randomizer * 10);
+
+
+
+
+
+const generateAccounts = (studentList, randomizerList) => {
+
+    const generateEmail = (student) => {
+        const firstName = removeSpaces(student.firstName);
+        const lastName = removeSpaces(student.lastName);
+        const idNum = student.idNum.toString().slice(0, 2);
+        return firstName.toLowerCase() + '.' + lastName.toLowerCase() + idNum + '@cpu.edu.ph';
     }
 
-    function generatePassword() {
-        const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        const punctutations = ['.', '!', '?'];
-
-        let randomLetters = new Array(8).fill('').map(() => letters[Math.trunc(Math.random() * letters.length)]);
-
-        let randomNumber = new Array(2).fill('').map(() => Math.trunc(Math.random() * 10));
-
-        let randomPunctutation = punctutations[Math.trunc(Math.random() * 3)];
-
-        return randomLetters.join('') + randomNumber.join('') + randomPunctutation;
-        //8 random letters, 2 random numbers, and one of the sentence-ending punctuations (period, exclamation, question mark).
-
-
-        // randomLetter = "a".charCodeAt(0) //=>97
+    const generatePassword = (radomizer11) => {
+        const lettersNumbers = radomizer11.slice(0, 9);
+        const numberNumbers = radomizer11.slice(8, 10);
+        const randomLetters = lettersNumbers.map(randomLetter);
+        const randomNumbers = numberNumbers.map(randomDigit);
+        const randomSymbol = randomPunctutation(radomizer11[10]);
+        return randomLetters.join('') + randomNumbers.join('') + randomSymbol;
     }
+    const indexOf = (student) => studentList.indexOf(student);
 
-    const generateAccount = function generate(student) {
-        return { email: generateEmail(student), password: generatePassword() };
+
+    const generateAccount = (student) => {
+        return { email: generateEmail(student), password: generatePassword(randomizerList[indexOf(student)]) };
+
     }
 
 
@@ -57,29 +56,99 @@ function generateAccounts(studentList) {
 
 
 
+const studentList = [
+    { firstName: 'Juan', lastName: 'Dela cruz', idNum: 20047798, course: 'BSSE', year: 2 },
+    { firstName: 'Pedro', lastName: 'Luna', idNum: 19039796, course: 'BSSS', year: 3 },
+    { firstName: 'Luffy', lastName: 'Monkey', idNum: 21152364, course: 'BSIT', year: 1 },
+    { firstName: 'Ace', lastName: 'Protgas', idNum: 20058843, course: 'BSSS', year: 2 },
+    { firstName: 'Juan Pedro', lastName: 'Luna', idNum: 20036723, course: 'BSN', year: 2 },
+    { firstName: 'John', lastName: 'Snow', idNum: 21095467, course: 'BSA', year: 1 }
+]
+
+const randomNumberList = [
+    [
+        0.5158915602392262,
+        0.8071955989616599,
+        0.11874190942192109,
+        0.28117735275524347,
+        0.06116048536018992,
+        0.5209073783062208,
+        0.4026296054378782,
+        0.6822096467616339,
+        0.8673377147612029,
+        0.29549446470841634,
+        0.57626164246533
+    ],
+    [
+        0.07599625519007591,
+        0.8405138663260152,
+        0.820667277222505,
+        0.05846083713528727,
+        0.8128933966976057,
+        0.4257551068742782,
+        0.23538750440187117,
+        0.4096200728688917,
+        0.7069796176550716,
+        0.20055829708853645,
+        0.29977855602440906
+    ],
+    [
+        0.839750607062079,
+        0.22244151062850337,
+        0.15644189995677893,
+        0.8059604012780497,
+        0.6386468732413819,
+        0.10478037223199532,
+        0.00834128053661054,
+        0.766258461797025,
+        0.5529712790244148,
+        0.5504291976372735,
+        0.12723304422470805
+    ],
+    [
+        0.6223969697885792,
+        0.8513795613315349,
+        0.2966935696701243,
+        0.3087290362610309,
+        0.3085434781395928,
+        0.7957527387226997,
+        0.09334964430177228,
+        0.41316398493429984,
+        0.15131513430327415,
+        0.9981509079216055,
+        0.30303425558949715
+    ],
+    [
+        0.9102019495764031,
+        0.8128883189210185,
+        0.7937307293368505,
+        0.7951622980896291,
+        0.09031976369644212,
+        0.91213264552263,
+        0.8582372838495436,
+        0.26903742128943886,
+        0.01121325683972585,
+        0.9019851241776107,
+        0.9016709341180611
+    ],
+    [
+        0.8462196518755072,
+        0.4525021047422242,
+        0.9186015428856058,
+        0.012159658543143825,
+        0.3713109154411054,
+        0.4611897664189546,
+        0.017600438382614936,
+        0.605481256529107,
+        0.764502449062141,
+        0.061242024929226035,
+        0.596308390020885
+    ]
+]
+
+console.log("year 2 students", getStudentsInYear(2, studentList));
+console.log("year 1 students", getStudentsInYear(1, studentList));
+console.log('accounts', generateAccounts(studentList, randomNumberList));
 
 
-printStundents(getStudentsInYear(1, studentList));
-printStundents(getStudentsInYear(2, studentList));
 
-printStundentsAccounts(generateAccounts(studentList));
-
-
-function printStundents(studentList) {
-    const print = (student) => {
-        console.log(`Student Name:     ${student.firstName} ${student.lastName}`)
-        console.log(`Student ID Num:   ${student.idNum}`);
-        console.log(`Course and Year:  ${student.course}-${student.year}\n`);
-    }
-    studentList.map(print);
-    console.log('\n \n \n');
-}
-
-function printStundentsAccounts(studentList) {
-    const print = (student) => {
-        console.log(`Student Email:     ${student.email}`)
-        console.log(`Student Password:  ${student.password} \n`);
-    }
-    studentList.map(print);
-    console.log('\n\n');
-}
