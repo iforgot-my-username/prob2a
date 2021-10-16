ae41253ccb9c0af600d061e550f9ec1d790f3234
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-16 18:23:07 +0800

    apply v2 of coupon.js file

--- number_3/coupon.js
+++ number_3/coupon.js
@@ -3,37 +3,38 @@ const isMatch = (regExp, str) => str.match(regExp) != null;
 const round = (num, places) => Number.isInteger(num) ? num : Math.round(num * 10 ** places) / 10 ** places;
 
 const billOut = (cart, applyCoupon) => {
-    let microsoftProducts = 0;
-    let microsoftProductsPrice = 0;
     const discount = 0.15;
     const addAllPrice = (total, current) => {
-        const totalPrice = getPrice(total);
-        const currentPrice = getPrice(current);
         const microsoft = /microsoft/gi;
-
+        let microsoftProducts = 0;
+        let microsoftProductsPrice = 0;
         if (isMatch(microsoft, current.seller)) {
             microsoftProducts++;
             microsoftProductsPrice += current.price;
         }
-        return totalPrice + currentPrice;
+        return total != undefined ? {
+            microsoftProducts: total.microsoftProducts + microsoftProducts,
+            microsoftProductsPrice: microsoftProductsPrice + total.microsoftProductsPrice,
+            bill: current.price + total.bill
+        } :
+            {
+                microsoftProducts: microsoftProducts,
+                microsoftProductsPrice: microsoftProductsPrice,
+                bill: current.price
+            };
     }
-
-    const summary = cart.reduce(addAllPrice, 0);
-    console.log(microsoftProducts);
-    if (applyCoupon && microsoftProducts >= 3 && microsoftProductsPrice > 6000) {
-        return round(summary - (summary * discount));
+    const summary = cart.reduce(addAllPrice, undefined);
+    if (applyCoupon && summary.microsoftProducts >= 3 && summary.microsoftProductsPrice > 6000) {
+        return round(summary.bill - (summary.bill * discount));
     }
-    return round(summary);
-
-
+    return round(summary.bill);
 }
 
-//test =>>>>>
+
 const cart1 = [
     { productName: 'microsoft word', seller: 'microsoft', price: 1500 },
     { productName: 'microsoft powerPoint', seller: 'microsoft', price: 3000 },
-    { productName: 'microsoft powerPoint', seller: 'microsoft', price: 2000 },
-];
-
-
-console.log(billOut(cart1, true));
+    { productName: 'microsoft powerPoint', seller: 'microsoFt', price: 2000 },
+    { productName: 'adobe dc', seller: 'adobe', price: 300 },
+]
+console.log(billOut(cart1, true))
