b334c387ef5289c3250418d6975c69f31804efbe
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-16 18:23:38 +0800

    apply v3 of coupon.js file

--- number_3/coupon.js
+++ number_3/coupon.js
@@ -1,40 +1,34 @@
+
+
 const getPrice = (item) => typeof item === 'number' ? item : item.price;
 const isMatch = (regExp, str) => str.match(regExp) != null;
 const round = (num, places) => Number.isInteger(num) ? num : Math.round(num * 10 ** places) / 10 ** places;
 
 const billOut = (cart, applyCoupon) => {
-    const discount = 0.15;
-    const addAllPrice = (total, current) => {
-        const microsoft = /microsoft/gi;
-        let microsoftProducts = 0;
-        let microsoftProductsPrice = 0;
-        if (isMatch(microsoft, current.seller)) {
-            microsoftProducts++;
-            microsoftProductsPrice += current.price;
+    let microsoftCount = 0;
+    let microsoftProductsTotal = 0;
+    let total = 0;
+    const microsoft = /microsoft/gi;
+    cart.forEach(element => {
+        if (isMatch(microsoft, element.seller)) {
+            microsoftCount++;
+            microsoftProductsTotal += element.price;
         }
-        return total != undefined ? {
-            microsoftProducts: total.microsoftProducts + microsoftProducts,
-            microsoftProductsPrice: microsoftProductsPrice + total.microsoftProductsPrice,
-            bill: current.price + total.bill
-        } :
-            {
-                microsoftProducts: microsoftProducts,
-                microsoftProductsPrice: microsoftProductsPrice,
-                bill: current.price
-            };
+        total += element.price;
+    });
+    const discount = 0.15;
+    if (applyCoupon && microsoftCount >= 3 && microsoftProductsTotal >= 6000) {
+        return round(total - (total * discount));
     }
-    const summary = cart.reduce(addAllPrice, undefined);
-    if (applyCoupon && summary.microsoftProducts >= 3 && summary.microsoftProductsPrice > 6000) {
-        return round(summary.bill - (summary.bill * discount));
-    }
-    return round(summary.bill);
+    return total;
 }
 
 
+//test =>>>>>
 const cart1 = [
-    { productName: 'microsoft word', seller: 'microsoft', price: 1500 },
-    { productName: 'microsoft powerPoint', seller: 'microsoft', price: 3000 },
-    { productName: 'microsoft powerPoint', seller: 'microsoFt', price: 2000 },
+    { productName: 'microsoft word', seller: 'microsoft', price: 6500 },
+    { productName: 'microsoft powerPoint', seller: 'microsoft', price: 200 },
+    { productName: 'microsoft powerPoint', seller: 'microsoFt', price: 300 },
     { productName: 'adobe dc', seller: 'adobe', price: 300 },
-]
-console.log(billOut(cart1, true))
+];
+console.log(billOut(cart1, true));
