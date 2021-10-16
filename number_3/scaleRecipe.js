b40a3c9616f0a35725f30ac6c228ad2b524f1e30
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-16 17:26:37 +0800

    Added scale a recipe solution

---
+++ number_3/scaleRecipe.js
@@ -0,0 +1,24 @@
+const round = (num, places) => Number.isInteger(num) ? num : Math.round(num * 10 ** places) / 10 ** places;
+
+const sizeUp = (newServing, recipe) => {
+    const ingredients = recipe.ingredients;
+    const scale = newServing / recipe.serves;
+    const changeQuantitiy = (ingredient) => {
+        const jsonStr = JSON.stringify(ingredient);
+        const quantity = /"quantity":\d+(\.\d+)?/gi;
+        const newQuantity = '"quantity":' + round(ingredient.quantity * scale, 2)
+        const newJsonStr = jsonStr.replace(quantity, newQuantity);
+        return JSON.parse(newJsonStr);
+    }
+    const newIngredients = ingredients.map(changeQuantitiy);
+    return { name: recipe.name, serves: newServing, ingredients: newIngredients };
+    // changeQuantitiy({ quantity: 500.5, unit: 'g', name: 'ground beef' })
+}
+
+const Spaghetti = {
+    name: 'Spaghetti', serves: 3, ingredients: [
+        { quantity: 500.5, unit: 'g', name: 'ground beef' },
+        { quantity: 500, unit: 'g', name: 'pasta' },
+        { quantity: 1, unit: 'pc', name: 'onion' }]
+}
+console.log(sizeUp(6, Spaghetti))
